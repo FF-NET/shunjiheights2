31767735548677b867565740a3998c356aa58a55
FF-NET <166034446+FF-NET@users.noreply.github.com>
2024-12-11 09:53:16 +0900

    챗박스삭제

--- js/plugins.js
+++ js/plugins.js
@@ -15,7 +15,7 @@ var $plugins =
 {"name":"MMO_Core_Party","status":true,"description":"MMORPG Maker MV - Core Handling Parties","parameters":{}},
 {"name":"MMO_Core_Party_Combat","status":false,"description":"MMORPG Maker MV - Core Handling Combat in Party","parameters":{}},
 {"name":"MMO_Core_Bank","status":false,"description":"MMORPG Maker MV - Core Handling Bank","parameters":{}},
-{"name":"MMO_ChatBox","status":true,"description":"MMORPG Maker MV - ChatBox","parameters":{"chatPosition":"BOTTOM CENTER"}},
+{"name":"MMO_ChatBox","status":false,"description":"MMORPG Maker MV - ChatBox","parameters":{"chatPosition":"BOTTOM CENTER"}},
 {"name":"MMO_LoginForm","status":true,"description":"MMORPG Maker MV - Login Form","parameters":{"allowAccountCreation":"No"}},
 {"name":"MMO_Overhead","status":false,"description":"Add name over actors name","parameters":{}},
 {"name":"CacheOverride","status":true,"description":"This allows to force users to re-load old files","parameters":{"gameVersion":"1.0.0"}},
